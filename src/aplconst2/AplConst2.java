107cf3c0fb96552c10f159ca539dffae3430fd30
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-10-15 10:41:58 +0200

    permisitos

---
+++ src/aplconst2/AplConst2.java
@@ -0,0 +1,40 @@
+package aplconst2;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class AplConst2 {
+
+    public static void main(String[] args) {
+        /*float base, altura;
+        Scanner obx=new Scanner(System.in);
+        System.out.println("indica base : ");
+        base=obx.nextFloat();
+        System.out.println("indica altura : ");
+        altura=obx.nextFloat();
+        Rectangulo rec1= new Rectangulo(base,altura);
+        System.out.println("base : "+rec1.getBase());
+        System.out.println("altura : "+rec1.getAltura());
+        System.out.println("area distinta = "+ rec1.calcularArea(7, 8));
+        System.out.println("perimetro = "+rec1.calcularPerimetro());*/
+             
+                
+        float base, altura;
+        String resposta=JOptionPane.showInputDialog("base :");
+        base= Float.parseFloat(resposta);
+        String resposta2=JOptionPane.showInputDialog("altura :");
+        altura=Float.parseFloat(resposta2);
+        JOptionPane.showMessageDialog (null,"base ="+ base); 
+        JOptionPane.showMessageDialog (null, "altura= "+ altura);
+        Rectangulo rec1= new Rectangulo(base,altura);
+        
+        JOptionPane.showMessageDialog (null, "area = "+ base*altura);
+        JOptionPane.showMessageDialog (null, "perimetro = "+ 2*(base+altura));    
+        
+        
+       
+   
+        
+    }
+    
+}
